b3d3ec887715230830e5436172234e75253b4161
edhinrichsen <edward.w.hinrichsen@gmail.com>
2020-07-20 22:48:00 +1000

    added menu

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function closeMenu(){
+    document.getElementById('menuToggle').checked = false;
+}
+
+function toggleMenu(){
+    document.getElementById('menuToggle').checked = !document.getElementById('menuToggle').checked;
+}
